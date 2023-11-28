d5841b403939e816d18515cf6cf178e77e1568b4
Yilong Qin <yilongq@cs.cmu.edu>
2023-11-28 04:14:55 +0000

    Add pretrained model paths.

--- collect_stats.py
+++ collect_stats.py
@@ -8,23 +8,43 @@ from torchvision.models import resnet50, ResNet50_Weights
 from datasets import get_group_loaders 
 from stats.stats_manager import StatsManager
 
+MODEL_PATHS = {
+	'fmow': 'models/FMoW/resnet50/pretrained/base_model_1-50.pt',
+	'camelyon17': 'models/Camelyon17/resnet50/pretrained/base_model_10-5.pt',
+	'rxrx1': 'models/RxRx1/resnet50/pretrained/base_model_10-90.pt',
+	'iwildcam': 'models/iWildCam/resnet50/pretrained/base_model_1-5.pt'
+}
+
 
 def load_model(dataset_name):
-	return resnet50(weights=ResNet50_Weights)
+	if dataset_name in ['imagenet-c']:
+		return resnet50(weights=ResNet50_Weights)
+	elif dataset_name in MODEL_PATHS:
+		return torch.load(MODEL_PATHS[dataset_name])['model'].module
+	else:
+		raise ValueError(f'Invalid dataset name {dataset_name}.')
+
+
+def adapt_model(model, adaptor=None):
+	return model
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--dataset', required=True)
+	parser.add_argument('--out-dir', default='resnet_intermediates')
 	args = parser.parse_args()
 
-	print('Loading model...')
-	model = load_model(args.dataset)
 	print('Loading data loaders...')
 	group_loaders = get_group_loaders(args.dataset, batch_size=100)
 	
 	for group_id, data_loader in group_loaders.items():
 		print(group_id)
+		print('Loading model...')
+		model = load_model(args.dataset)
+		print('Adapting model...')
+		model = adapt_model(model)
+		
 		stats_manager = StatsManager(model, data_loader, 'cuda')
 		stats_manager.collect_stats()
-		stats_manager.save_stats(f'{args.dataset}_{group_id}')
-		breakpoint()
+		stats_manager.save_stats(
+			f'{args.out_dir.strip()}/{args.dataset}/{group_id}')
