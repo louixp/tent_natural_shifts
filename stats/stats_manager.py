65c42a71d74fdf82fbe82a586930c4f5b4d2fbc8
Yilong Qin <yilongq@cs.cmu.edu>
2023-12-02 04:38:25 +0000

    Add online mean update; compute stats on gpu.

--- stats/stats_manager.py
+++ stats/stats_manager.py
@@ -24,112 +24,131 @@ class StatsManager:
                 module.inplace = False
 
         self.data_loader = data_loader
-        self.batch_size = data_loader.batch_size
-        self.n_batches = len(data_loader)
         self.device = device
 
         self._setup_hooks()
-        self.gradients_mean = dict()
-        self.gradients_2nd_moment = dict()
+        self.gradients_mean = defaultdict(float)
+        self.gradients_2nd_moment = defaultdict(float)
     
     def collect_stats(self) -> None:
-        for x, y_true, _ in tqdm.tqdm(self.data_loader):
-            if len(x) < self.batch_size:
-                self.n_batches -= 1
-                break
+        self.n_batch = 0
+        for x, y_true, _ in (pbar := tqdm.tqdm(self.data_loader)):
+            self.n_batch += 1
+            # self.relu_invocation_counter = defaultdict(int)
+            self.max_forward_update = 0
+            self.max_backward_update = 0
+            self.max_grad_update = 0
             
-            self.relu_invocation_counter = defaultdict(int)
             x.requires_grad = True # hack for gradient checkpointing
             y_pred = checkpoint(self.model, x.to(self.device))
             loss = F.cross_entropy(y_pred, y_true.to(self.device))
             loss.backward()
 
             self._save_gradients()
+            self.model.zero_grad()
+
+            pbar.set_description(f'max update|fwd{self.max_backward_update:.0e}|bwk{self.max_backward_update:.0e}|grad{self.max_grad_update:.0e}')
     
     def save_stats(self, path: str) -> None:
         if not os.path.exists(path):
             os.makedirs(path)
 
-        intermediates_attrs = [
+        attrs = [
             'forward_intermediates_mean', 
             'forward_intermediates_2nd_moment', 
             'backward_intermediates_mean', 
             'backward_intermediates_2nd_moment',
-        ]
-
-        for attr in intermediates_attrs:
-            with open(os.path.join(path, f'{attr}.pickle'), 'wb') as fp:
-                pickle.dump(
-                    {
-                        k: v / self.batch_size / self.n_batches 
-                        for k, v in getattr(self, attr).items()
-                    },
-                    fp
-                )
-
-        gradients_attrs = [
             'gradients_mean',
             'gradients_2nd_moment'
         ]
 
-        for attr in gradients_attrs:
+        for attr in attrs:
             with open(os.path.join(path, f'{attr}.pickle'), 'wb') as fp:
                 pickle.dump(
                     {
-                        k: v / self.n_batches 
+                        k: v.cpu().numpy()
                         for k, v in getattr(self, attr).items()
-                    },
-                    fp
-                )
+                    }, fp)
 
     def _setup_hooks(self):
-        self.forward_intermediates_mean = dict() 
-        self.forward_intermediates_2nd_moment = dict()
+        self.forward_intermediates_mean = defaultdict(float)
+        self.forward_intermediates_2nd_moment = defaultdict(float)
+        self.activation_probability = defaultdict(float)
         
-        self.backward_intermediates_mean = dict()
-        self.backward_intermediates_2nd_moment = dict()
+        self.backward_intermediates_mean = defaultdict(float)
+        self.backward_intermediates_2nd_moment = defaultdict(float)
 
         for name, module in self.model.named_modules():
             if isinstance(module, Bottleneck) or name == 'fc':
                 def forward_hook(module, input, output, name=name):
-                    output_np = output.detach().cpu().numpy()
-                    mean = output_np.sum(axis=0)
-                    second_moment = np.square(output_np).sum(axis=0)
-                    if isinstance(module, nn.ReLU):
-                        self.relu_invocation_counter[name] += 1
-                        name += str(self.relu_invocation_counter[name])
-                    if name not in self.forward_intermediates_mean:
-                        self.forward_intermediates_mean[name] = np.zeros_like(mean)
-                    if name not in self.forward_intermediates_2nd_moment:
-                        self.forward_intermediates_2nd_moment[name] = np.zeros_like(second_moment)
+                    output_detached = output.detach()
+                    mean = output_detached.mean(0)
+                    second_moment = (output_detached ** 2).mean(0)
+                    # if isinstance(module, nn.ReLU):
+                    #     self.relu_invocation_counter[name] += 1
+                    #     name += str(self.relu_invocation_counter[name])
 
-                    self.forward_intermediates_mean[name] += mean
-                    self.forward_intermediates_2nd_moment[name] += second_moment
+                    mean_update = (
+                        mean - self.forward_intermediates_mean[name]
+                    ) / self.n_batch
+                    self.forward_intermediates_mean[name] += mean_update
+
+                    second_moment_update = (
+                        second_moment - self.forward_intermediates_2nd_moment[name]
+                    ) / self.n_batch 
+                    self.forward_intermediates_2nd_moment[name] += second_moment_update
+                    
+                    self.max_forward_update = max(
+                        mean_update.abs().max(),
+                        second_moment_update.abs().max(),
+                        self.max_forward_update
+                    )
+
+                    if isinstance(module, Bottleneck):
+                        self.activation_probability[name] = (output_detached != 0).float().mean(0)
+                    
                 module.register_forward_hook(forward_hook)
                 
                 def backward_hook(module, grad_input, grad_output, name=name):
-                    grad_output_np = grad_output[0].detach().cpu().numpy()
-                    mean = grad_output_np.sum(axis=0)
-                    second_moment = np.square(grad_output_np).sum(axis=0)
-                    if isinstance(module, nn.ReLU):
-                        self.relu_invocation_counter[name] += 1
-                        name += str(self.relu_invocation_counter[name])
-                    if name not in self.backward_intermediates_mean:
-                        self.backward_intermediates_mean[name] = np.zeros_like(mean)
-                    if name not in self.backward_intermediates_2nd_moment:
-                        self.backward_intermediates_2nd_moment[name] = np.zeros_like(second_moment)
+                    grad_output_detached = grad_output[0].detach()
+                    mean = grad_output_detached.mean(0)
+                    second_moment = (grad_output_detached ** 2).mean(0)
+                    # if isinstance(module, nn.ReLU):
+                    #     self.relu_invocation_counter[name] += 1
+                    #     name += str(self.relu_invocation_counter[name])
 
-                    self.backward_intermediates_mean[name] += mean
-                    self.backward_intermediates_2nd_moment[name] += second_moment
+                    mean_update = (
+                        mean - self.backward_intermediates_mean[name]
+                    ) / self.n_batch
+                    self.backward_intermediates_mean[name] += mean_update
+
+                    second_moment_update = (
+                        second_moment - self.backward_intermediates_2nd_moment[name]
+                    ) / self.n_batch
+                    self.backward_intermediates_2nd_moment[name] += second_moment_update
+
+                    self.max_backward_update = max(
+                        mean_update.abs().max(),
+                        second_moment_update.abs().max(),
+                        self.max_backward_update
+                    )
+
                 module.register_full_backward_hook(backward_hook)
     
     def _save_gradients(self):
         for name, parameter in self.model.named_parameters():
-            grad = parameter.grad.cpu().numpy()
-            if name not in self.gradients_mean:
-                self.gradients_mean[name] = np.zeros_like(grad)
-            if name not in self.gradients_2nd_moment:
-                self.gradients_2nd_moment[name] = np.zeros_like(grad)
-            
-            self.gradients_mean[name] += grad
-            self.gradients_2nd_moment[name] += grad ** 2
+            mean_update = (
+                parameter.grad - self.gradients_mean[name]
+            ) / self.n_batch
+            self.gradients_mean[name] += mean_update
+
+            second_moment_update = (
+                parameter.grad ** 2 - self.gradients_2nd_moment[name]
+            ) / self.n_batch
+            self.gradients_2nd_moment[name] += second_moment_update
+
+            self.max_grad_update = max(
+                mean_update.abs().max(),
+                second_moment_update.abs().max(),
+                self.max_grad_update
+            )
